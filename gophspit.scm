ca58a1d7118665e8ed09e8a4a643e38b0b635176
Chris Murphy <camurp02009@gmail.com>
2016-06-11 22:23:19 -0400

    Rendering of downloaded gopherholes works.

--- gophspit.scm
+++ gophspit.scm
@@ -10,40 +10,42 @@ exec /usr/bin/env guile-2.0 -e main -s "$0" "$@"
 
 (define line-regexp
   (make-regexp
-    "^([0-9iI])([^\t]*)\t([^\t]*)\t([^\t]*)\t([^\t]*).*"
+    "^(.)([^\t]*)\t([^\t]*)\t([^\t]*)\t([^\t]*).*"
     regexp/extended))
 
 (define (translate-line line)
-  (let ((match (regexp-exec line-regexp line)))
-    (let ((type     (match:substring match 1))
-          (text     `(pre ,(match:substring match 2)))
-          (selector (match:substring match 3))
-          (addr     (match:substring match 4))
-          (port     (match:substring match 5)))
-      `(div (@ (class ,(string-append "line type-"
-                                      (if (char-upper-case?
-                                            (string-ref type 0))
-                                        (string-append "cap-" type)
-                                        type))))
-         ,@(cond
-             ((string=? type "i")
-               `(,text))
-             ((or (string=? type "0")
-                  (string=? type "1")
-                  (string=? type "I"))
-                `((a (@ (href ,(cond
-                                 ((and (string=? addr "(ROOT)")
-                                       (string=? port "(ROOT)"))
-                                   selector)
-                                 ((and (string=? addr "(HERE)")
-                                       (string=? port "(HERE)"))
-                                   (string-append "." selector))
-                                 (else (string-append
-                                         "http://"
-                                         addr ":" port selector)))))
-                    ,text)))
-             (else
-               `(,text)))))))
+  (if (string=? line ".\r")
+    ""
+    (let ((match (regexp-exec line-regexp line)))
+      (let ((type     (match:substring match 1))
+            (text     `(pre ,(match:substring match 2)))
+            (selector (match:substring match 3))
+            (addr     (match:substring match 4))
+            (port     (match:substring match 5)))
+        `(div (@ (class ,(string-append "line type-"
+                                        (if (char-upper-case?
+                                              (string-ref type 0))
+                                          (string-append "cap-" type)
+                                          type))))
+           ,@(cond
+               ((string=? type "i")
+                 `(,text))
+               ((or (string=? type "0")
+                    (string=? type "1")
+                    (string=? type "I"))
+                  `((a (@ (href ,(cond
+                                   ((and (string=? addr "(ROOT)")
+                                         (string=? port "(ROOT)"))
+                                     selector)
+                                   ((and (string=? addr "(HERE)")
+                                         (string=? port "(HERE)"))
+                                     (string-append "." selector))
+                                   (else (string-append
+                                           "http://"
+                                           addr ":" port selector)))))
+                      ,text)))
+               (else
+                 `(,text))))))))
 
 (define (translate port)
   (with-output-to-string
